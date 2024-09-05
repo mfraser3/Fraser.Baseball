1027c34c956c5b80e523ab0c52f540ccf17632cc
mfraser3 <michaelfraser@gmail.com>
2024-09-05 11:40:14 -0400

    Modify function to allow different paths

--- R/get_Lahman_data.R
+++ R/get_Lahman_data.R
@@ -1,11 +1,12 @@
 ## GET LAHMAN DATA FOR SELECTED YEAR FOR SPECIFIC TABLES ##
 
-get_Lahman <- function(yearID, table) { ## YEARID IS THE LATEST YEAR TO INCLUDE; TABLE IS THE DATASET OF INTEREST (e.g. Batting, Pitching, HallOfFame, etc)
+get_Lahman <- function(yearID, table, path = "~/Nextcloud/Personal/Data/Baseball/Lahman/") { ## YEARID IS THE LATEST YEAR TO INCLUDE; TABLE IS THE DATASET OF INTEREST (e.g. Batting, Pitching, HallOfFame, etc)
 
   yearID <- yearID
 
-  path <- paste0("~/Nextcloud/Personal/Data/Baseball/Lahman/", yearID)
+  path <- paste0(path, yearID)
 
   df <- read_csv(paste0(path, "/", table, ".csv"))
 
 }
+
